3ef8dcd1a531f678c51d781f9dcb2205be7fe774
imil4eg <emzilh@mail.ru>
2017-11-01 20:23:43 +0400

    Add project files.

---
+++ ReserveSystem/HoleWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ReserveSystem
+{
+    /// <summary>
+    /// Interaction logic for HoleWindow.xaml
+    /// </summary>
+    public partial class HoleWindow : Window
+    {
+        public HoleWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ReserveWindow rw = new ReserveWindow();
+            rw.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Orders ow = new Orders();
+            ow.Show();
+        }
+    }
+}
---
+++ ReserveSystem/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ReserveSystem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (LoginBox.Text == "admin" && PasswordBox.Text == "admin")
+            {
+                HoleWindow hw = new HoleWindow();
+                hw.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин и пароль");
+            }
+        }
+
+    }
+}
